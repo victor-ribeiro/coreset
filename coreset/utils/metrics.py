3b359d79aa07be44161e138d418e1564d1636349
victor-ribeiro <ribeiro.victor.dornellas@gmail.com>
2024-08-12 11:21:44 -0300

    coreset prototype

--- coreset/utils/metrics.py
+++ coreset/utils/metrics.py
@@ -2,13 +2,14 @@ import numpy as np
 from sklearn.metrics import pairwise_distances
 
 
-# def similarity(pdist):
-#     def inner(*args, **kwargs):
-#         yield from [D.max() - D for D in pdist(*args, **kwargs)]
+def similarity(dist):
+    def inner(*args, **kwargs):
+        yield from [D.max() - D for D in dist(*args, **kwargs)]
 
-#     return inner
+    return inner
 
 
+# @similarity
 def pdist(dataset, metric="euclidean", batch_size=1):
     n = len(dataset)
     for start in range(0, n, batch_size):
