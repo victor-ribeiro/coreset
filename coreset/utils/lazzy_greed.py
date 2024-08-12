6a0a2d8b7f14d3a2b2ebd77215143822a0ac410e
victor-ribeiro <ribeiro.victor.dornellas@gmail.com>
2024-08-12 11:20:47 -0300

    coreset prototype

--- coreset/utils/lazzy_greed.py
+++ coreset/utils/lazzy_greed.py
@@ -1,28 +1,14 @@
-#############################################
-### |S| <= 1 + log max F( e | [] ) | S* | ###
-#############################################
+###############################################
+### |S| <= 1 + log max F( e | [] ) x | S* | ###
+###############################################
 
 from typing import Callable
-import numpy as np
 import heapq
+import numpy as np
+from collections import UserList
 
 from coreset.utils.dataset import Dataset
 from coreset.utils.metrics import pdist
-
-
-def facility_location(subset: np.array):
-    return subset.max(axis=1).sum()
-
-
-def lazzygreed(
-    dataset, size: int | float, tol: float, utility_function: Callable, sample=1
-):
-    if not bool(size) ^ bool(tol):
-        raise ValueError
-
-
-def submodular(dataset):
-    pass
 
 
 class Coreset(Dataset):
@@ -35,3 +21,37 @@ class Coreset(Dataset):
 
     def __radd__(self, elemen):
         return self.__add__(elemen)
+
+
+def _push(heap, e):
+    heap.append(e)
+    heapq._siftdown_max(heap, e, len(heap))
+
+
+def queue(V, util_func=None):
+    order = []
+    heapq.heapify(order)
+    [_push(order, i) for i in range(len(V))]
+    yield from order
+
+
+def lazzygreed(
+    dataset, size: int | float, tol: float, utility_function: Callable, sample=1
+):
+    if not bool(size) ^ bool(tol):
+        raise ValueError
+    for D in pdist(dataset):
+        pass
+
+
+if __name__ == "__main__":
+    import numpy as np
+
+    new = []
+
+    q = np.random.normal(0, 100, 10).astype(int).tolist()
+    q = queue(q)
+    for coisa in q:
+        print(coisa)
+
+    # print(q)
--- coreset/utils/metrics.py
+++ coreset/utils/metrics.py
@@ -2,6 +2,13 @@ import numpy as np
 from sklearn.metrics import pairwise_distances
 
 
+# def similarity(pdist):
+#     def inner(*args, **kwargs):
+#         yield from [D.max() - D for D in pdist(*args, **kwargs)]
+
+#     return inner
+
+
 def pdist(dataset, metric="euclidean", batch_size=1):
     n = len(dataset)
     for start in range(0, n, batch_size):
