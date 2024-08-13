7ff218ac64fb5898523765ade1898021e8841520
victor-ribeiro <ribeiro.victor.dornellas@gmail.com>
2024-08-13 16:50:06 -0300

    coreset prototype

--- tests/utils/test_metrics.py
+++ tests/utils/test_metrics.py
@@ -8,14 +8,13 @@ from coreset.utils.dataset import Dataset
 
 @lambda _: _()
 def new_dataset():
-    return np.random.normal(0, 1, (10, 3))
+    return np.random.normal(0, 1, (10, 4))
 
 
 def test_custom_dataset_euclidean_distance():
     dataset = pd.DataFrame(new_dataset)
     D = pairwise_distances(dataset)
     _D = pdist(Dataset(dataset), batch_size=2)
-    # _D = pdist(dataset, batch_size=2)
     _D = np.vstack([*_D])
-    # assert np.allclose(D, _D)  # and D.shape == _D.shape
-    assert D.shape == _D.shape
+    assert np.allclose(D, _D) and D.shape == _D.shape
+    # assert D.shape == _D.shape
