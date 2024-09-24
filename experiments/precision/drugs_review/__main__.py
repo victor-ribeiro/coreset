fa7910c485eaed7317570a091bd6170fd6059af8
victor-ribeiro <ribeiro.victor.dornellas@gmail.com>
2024-09-24 16:38:20 -0300

    coreset prototype

--- experiments/precision/drugs_review/__main__.py
+++ experiments/precision/drugs_review/__main__.py
@@ -154,16 +154,13 @@ y_test = np.array(y_test)
 # plt.show()
 
 model = XGBClassifier(
-    objective="multi:softmax",
     # max_depth=10,
     # max_depth=4,
+    eta=0.2,
     early_stopping_rounds=2,
     n_estimators=1000,
     device="gpu",
     nthread=n_threads,
-    # eval_metric=["mlogloss", "merror"],
-    # eval_metric=["merror"],
-    eval_metric=["auc"],  # # -> atual
 )
 
 print(f"[TRAINING] ntread: {n_threads} :: x_shape: {X_train.shape}")
