c4aaf3a5ce103b82b9f73337bfda0c012ecefa34
victor-ribeiro <ribeiro.victor.dornellas@gmail.com>
2024-11-05 12:26:53 -0300

    coreset prototype

--- experiments/mnist/__main__.py
+++ experiments/mnist/__main__.py
@@ -45,7 +45,7 @@ result = []
 
 n, r, c = X_train.shape
 c = r * c
-reg, epochs, batch_size, core_size = 10e-4, 15, 256 * 4, 0.4
+reg, epochs, batch_size, core_size = 10e-4, 15, 256 * 2, 0.4
 
 X_train, X_test = X_train.reshape((n, c)), X_test.reshape((len(X_test), c))
 
